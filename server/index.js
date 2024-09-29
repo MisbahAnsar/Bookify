0234c59aa3a82ce8637af5aea4dd1c97518b20f6
MisbahAnsar <misbaansari444@gmail.com>
2024-09-29 14:36:23 +0530

    added

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:"https://bookify-peach.vercel.app/",
+    origin:"https://bookify-peach.vercel.app",
     credentials:true
 }
 ));
